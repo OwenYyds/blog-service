d074b97be0088bfe873a9173e52683b11c800e35
Owen Yang <youngwhenball@gmail.com>
2025-03-16 13:21:04 +0800

    add jwt validator exclude login/register

---
+++ src/main/java/com/owen/interceptors/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.owen.interceptors;
+
+import com.owen.utills.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle (HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		String token = request.getHeader("Authorization");
+
+		try {
+			Map<String, Object> claims = JwtUtil.verifyToken(token);
+
+			return true;
+		} catch (Exception e) {
+			response.setStatus(401);
+			return false;
+		}
+
+	}
+}
