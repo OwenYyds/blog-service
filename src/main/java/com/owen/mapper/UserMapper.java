08f655f0f6357e1f1f6ad4e83e991dc1ead12b29
Owen Yang <youngwhenball@gmail.com>
2025-03-16 16:59:53 +0800

    add JsonIgnore annotation to password field and simplify user queries

--- src/main/java/com/owen/mapper/UserMapper.java
+++ src/main/java/com/owen/mapper/UserMapper.java
@@ -10,12 +10,12 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-	@Select("select id, user_name as userName, password, email, phone, avatar, create_time as createTime, update_time as updateTime, last_login_time as lastLoginTime, status, deleted, role, token, description, nick_name as nickName from user  where user_name = #{userName}")
+	@Select("select * from user  where user_name = #{userName}")
 	User findByUserName(String userName);
 
 	@Insert("insert into user(user_name, password) values(#{userName}, #{password})")
 	void add(User user);
 
-	@Select("select id, user_name as userName, password, email, phone, avatar, create_time as createTime, update_time as updateTime, last_login_time as lastLoginTime, status, deleted, role, token, description, nick_name as nickName from user")
+	@Select("select *  from user")
 	List<User> findAll();
 }
